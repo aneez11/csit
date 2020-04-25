ff3b12245d9fa6034ab5f5f47713c81bb329ab02
Nish-ant-98 <61265924+Nish-ant-98@users.noreply.github.com>
2020-04-25 12:44:18 +0545

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                CSIT 2073
+                CSIT 2073 123
             </div>
 
             <div class="links">
